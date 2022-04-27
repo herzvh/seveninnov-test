fb6b86deb001fdcc0c516387531b67f6c0978b83
herz <herizovonjinirina@gmail.com>
2022-04-27 21:41:41 +0300

    Fix styles

--- src/pages/home/components/TimeSlotBanner.vue
+++ src/pages/home/components/TimeSlotBanner.vue
@@ -1,12 +1,12 @@
 <template>
-    <v-btn 
+    <v-container 
         small 
         elevation="0"
-        class="mt-2"
+        class="mt-2 v-btn__content text-wrap"
         :style="{ color, backgroundColor}"
     >
         {{ beginAtMoment }} - {{ endAtMoment }}
-    </v-btn>
+    </v-container>
 </template>
 <script>
 import moment from 'moment'
@@ -19,14 +19,14 @@ export default {
          */
         beginAt: {
             type: String,
-            default: moment().format('LLLL')
+            default: moment().format('llll')
         },
         /**
          * Date et heure fin du webinaire
          */
         endAt: {
             type: String,
-            default: moment().format('LLLL')
+            default: moment().format('llll')
         },
         /**
          * Couleur du texte par défaut c'est #cf9fd7
@@ -45,11 +45,17 @@ export default {
     },
     computed: {
         beginAtMoment() {
-            return moment(this.beginAt).format('LLLL')
+            return moment(this.beginAt).format('llll')
         },
         endAtMoment() {
-            return moment(this.endAt).format('LLLL')
+            return moment(this.endAt).format('llll')
         }
     }
 }
 </script>
+<style scoped>
+    .v-btn__content {
+        width: 100%; 
+        white-space: normal;
+    }
+</style>
